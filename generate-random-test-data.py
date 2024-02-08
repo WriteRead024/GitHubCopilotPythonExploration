59f81180312778eae18bcd24841ca99615ebe3e5
Rich W <riiichw02@gmail.com>
2024-02-08 17:46:37 -0500

    additional random data functionality

--- generate-random-test-data.py
+++ generate-random-test-data.py
@@ -10,3 +10,10 @@ import random
 def generate_random_number():
     return random.randint(1, 10)
 
+def generate_random_number_list(length):
+        return list(random.randint(1, 10) for _ in range(length))
+
+if __name__ == '__main__':
+    resultnumber = generate_random_number()
+    resultset = generate_random_number_list(resultnumber)
+    print(resultset)
