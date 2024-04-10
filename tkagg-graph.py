88a9d47e06c118ada97625329194ac73c4fd4614
WriteRead024 <rich@rwhifonline.net>
2024-04-10 14:25:44 -0400

    using font from controller, comments, exposing figure subplot

--- tkagg-graph.py
+++ tkagg-graph.py
@@ -22,21 +22,23 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 
-ARIAL_FONT = ("Arial", 12) ################ somehow import this from the controller/parent?
-
-
-class TkaggGraph(tk.Frame):
+class TkaggGraph(tk.Frame):    
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="Graph Page", font=controller.child_font)
         label.pack(padx=2,pady=2)
-        fig = Figure(figsize=(5, 5), dpi=100)
-        a_sp = fig.add_subplot(111)
-        a_sp.plot([1, 2, 3, 4, 5], [4, 4, 4, 4, 3])
-        #
-        canvas = FigureCanvasTkAgg(fig, self)
+
+        # instantiate and expose the figure and subplot as class instance attributes
+        self.fig = Figure(figsize=(5, 5), dpi=100)
+        self.a_sp = self.fig.add_subplot(111)
+        
+        # plot some default data
+        self.a_sp.plot([1, 2, 3, 4, 5], [4, 4, 4, 4, 3])
+
+        # instantiate and show the canvas
+        canvas = FigureCanvasTkAgg(self.fig, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
